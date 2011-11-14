a475842e31d46f47056b30093351101f3a6f2da4
Daniel Pelsmaeker <daniel.pelsmaeker@16tonweight.com>
2011-11-14 13:17:49 +0100

    + Expression classes

---
+++ Core Expressions/CurrentSectionExpression.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpAssembler.Core.Expressions
+{
+	/// <summary>
+	/// This expression evaluates to the virtual address of the start of the
+	/// section currently being assembled.
+	/// </summary>
+	public class CurrentSectionExpression : Expression
+	{
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CurrentSectionExpression"/> class.
+		/// </summary>
+		public CurrentSectionExpression()
+		{
+		}
+		#endregion
+
+		#region Methods
+		/// <summary>
+		/// Accepts a visitor.
+		/// </summary>
+		/// <param name="visitor">The <see cref="ExpressionVisitor"/> to accept.</param>
+		public override void Accept(ExpressionVisitor visitor)
+		{
+			visitor.VisitCurrentSectionExpression(this);
+		}
+		#endregion
+
+		#region Evaluation
+		/// <summary>
+		/// Evaluates the expression in the given context.
+		/// </summary>
+		/// <param name="context">The <see cref="IContext"/> in which this expression is found.</param>
+		/// <returns>The expression to which the expression evaluated.</returns>
+		public ExpressionResult Evaluate(IContext context)
+		{
+			#region Contract
+			Contract.Requires<ArgumentNullException>(context != null);
+			Contract.Requires<ArgumentNullException>(context.Section != null);
+			Contract.Ensures(Contract.Result<ExpressionResult>() != null);
+			#endregion
+
+			return new ExpressionResult(context.Section.AssociatedSymbol, 0);
+		}
+		#endregion
+	}
+}
---
+++ Core Expressions/UnaryOperation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpAssembler.Core.Expressions
+{
+	/// <summary>
+	/// Specifies the kind of unary operation performed on the operands.
+	/// </summary>
+	public enum UnaryOperation
+	{
+		/// <summary>
+		/// No operation.
+		/// </summary>
+		/// <remarks>Evaluates to 0.</remarks>
+		None,
+		/// <summary>
+		/// A negation.
+		/// </summary>
+		/// <remarks>Evaluates to -<see cref="UnaryExpression.Expression"/>.</remarks>
+		Negate = 1,	//0x0700,
+		/// <summary>
+		/// Does nothing.
+		/// </summary>
+		/// <remarks>Evaluates to <see cref="UnaryExpression.Expression"/>.</remarks>
+		Positivate = 2,	//0x0701,
+		/// <summary>
+		/// Increment.
+		/// </summary>
+		/// <remarks>Evaluates to <see cref="UnaryExpression.Expression"/>+1.</remarks>
+		Increment = 3,	//0x0800,
+		/// <summary>
+		/// Decrement.
+		/// </summary>
+		/// <remarks>Evaluates to <see cref="UnaryExpression.Expression"/>-1.</remarks>
+		Decrement = 4,	//0x0801,
+		/// <summary>
+		/// Bitwise complement.
+		/// </summary>
+		/// <remarks>Evaluates to ~<see cref="UnaryExpression.Expression"/>.</remarks>
+		Complement = 5,	//0x0703,
+		/// <summary>
+		/// Logical negation.
+		/// </summary>
+		/// <remarks>Evaluates to !<see cref="UnaryExpression.Expression"/>.</remarks>
+		Not = 6,		//0x0702,
+	}
+}
